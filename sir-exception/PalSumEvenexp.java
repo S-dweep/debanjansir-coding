a57c87e2635d1ee7570a4d78a723433cfd5f9d00
Sayandeep Mondal <mondalsayandeep2004@gmail.com>
2025-04-29 19:57:33 +0530

    29/4

---
+++ sir-exception/PalSumEvenexp.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class MyExp extends Exception {
+    public String msg() {
+        return "User defined exception: odd palindrome sum";
+    }
+}
+
+class PalSumEvenexp { 
+    public static void PalindromeSum(int arr[], int n) throws MyExp {
+        int num, sum=0;
+        for(int i=0;i<n;i++){
+            int rev=0;
+            num=arr[i];
+            while(num>0){
+                rev=(rev*10)+(num%10);
+                num/=10;
+            }
+            if(rev==arr[i]) sum+=arr[i];
+        }
+        if(sum%2==0) System.out.println("even palindrome sum");
+        else throw new MyExp();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        try {
+            PalindromeSum(arr,n);
+        } catch (MyExp e) {
+            System.out.println(e.msg());
+        } finally {
+            sc.close();
+        }
+    }
+}
